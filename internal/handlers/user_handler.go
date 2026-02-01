82364d7233ed623674fc122c84905a16e5505fce
alanloffler <alanmatiasloffler@gmail.com>
2026-01-31 23:47:18 -0300

    Create user handler

---
+++ internal/handlers/user_handler.go
@@ -0,0 +1,55 @@
+package handlers
+
+import (
+	"go-todo-api/internal/models"
+	"go-todo-api/internal/repository"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type RegisterRequest struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+func CreateUserHandler(pool *pgxpool.Pool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var registerRequest RegisterRequest
+
+		if err := c.BindJSON(&registerRequest); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+
+		if len(registerRequest.Password) < 6 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Password must be at least 6 characters long"})
+		}
+
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(registerRequest.Password), bcrypt.DefaultCost)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password " + err.Error()})
+		}
+
+		user := &models.User{
+			Email:    registerRequest.Email,
+			Password: string(hashedPassword),
+		}
+
+		createdUser, err := repository.CreateUser(pool, user)
+
+		if err != nil {
+			if err.Error() != "" {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "Email already registered"})
+				return
+			}
+
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusCreated, createdUser)
+	}
+}

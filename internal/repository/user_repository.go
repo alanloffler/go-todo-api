e1952bb46e99ae129a55b540b5680f53eef9f6eb
alanloffler <alanmatiasloffler@gmail.com>
2026-02-01 21:01:09 -0300

    Get user by email

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -63,6 +63,35 @@ func GetUserByEmail(pool *pgxpool.Pool, email string) (*models.User, error) {
 	return &user, nil
 }
 
+func GetUserByEmail(pool *pgxpool.Pool, email string) (*models.User, error) {
+	var ctx context.Context
+	var cancel context.CancelFunc
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var query string = `
+			SELECT id, email, password, created_at, updated_at
+			FROM users
+			WHERE email = $1
+	`
+
+	var user models.User
+
+	err := pool.QueryRow(ctx, query, email).Scan(
+		&user.ID,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
 func GetUserById(pool *pgxpool.Pool, id string) (*models.User, error) {
 	var ctx context.Context
 	var cancel context.CancelFunc
